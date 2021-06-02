db5d6a508368c2069e9a77d0bfcedbe26e6211e5
kevin <mail@kseidel.org>
2021-06-02 10:28:29 +0200

    Remove unsued :eex in extra_applications

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule EQRCode.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :eex]
+      extra_applications: [:logger]
     ]
   end
 
